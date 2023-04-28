686ca6c0b7eff7955c627c518f6e70b9cd074b16
saqib <saqib@saqib.com>
2023-04-28 19:26:57 +0200

    missing view models

---
+++ app/src/main/java/com/jetpackcompose/suspendfunctions/viewModels/LaunchedEffectTestViewModel.kt
@@ -0,0 +1,24 @@
+package com.jetpackcompose.suspendfunctions.viewModels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class LaunchedEffectTestViewModel : ViewModel() {
+
+    private var _snackbarCount = MutableStateFlow(1)
+    val snackbarCount: StateFlow<Int> get() = _snackbarCount
+
+    init {
+        viewModelScope.launch {
+            var displayCount = 1
+            while (displayCount < 3) {
+                delay(1000L)
+                displayCount += 1
+                _snackbarCount.value = displayCount
+            }
+        }
+    }
+}
